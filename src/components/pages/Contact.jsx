9b57854da2376e4a22a794bebf831ec99df0744a
Nixon Pastor <nixonpastorguzman@gmail.com>
2020-09-27 03:30:52 -0700

    fixed contact css

--- src/components/pages/Contact.jsx
+++ src/components/pages/Contact.jsx
@@ -24,42 +24,44 @@ export default function Contact() {
     e.target.reset();
   }
   return (
-    <div className="contactBackground">
-      <h1 className="contactHeader">Contact Me</h1>
-      <div className="container">
-        <form onSubmit={sendEmail}>
-          <div className="inputField">
-            <div className="col-8 form-group mx-auto">
-              <input type="text" placeholder="Your Name" name="name" />
-            </div>
+    <>
+      <div className="contactBackground">
+        <h1 className="contactHeader">Contact Me</h1>
+        <div className="container">
+          <form onSubmit={sendEmail}>
             <div className="inputField">
-              <input
-                type="text"
-                placeholder="Your Email Address"
-                name="email"
-              />
+              <div className="col-8 form-group mx-auto">
+                <input type="text" placeholder="Your Name" name="name" />
+              </div>
+              <div className="inputField">
+                <input
+                  type="text"
+                  placeholder="Your Email Address"
+                  name="email"
+                />
+              </div>
+              <div className="inputField">
+                <input type="text" placeholder="Subject" name="subject" />
+              </div>
+              <div className="textBox">
+                <textarea
+                  type="text"
+                  placeholder="Your Message"
+                  name="message"
+                ></textarea>
+              </div>
+              <div>
+                <input
+                  type="submit"
+                  className="btn--outline"
+                  value="Send Message"
+                ></input>
+              </div>
             </div>
-            <div className="inputField">
-              <input type="text" placeholder="Subject" name="subject" />
-            </div>
-            <div className="textBox">
-              <textarea
-                type="text"
-                placeholder="Your Message"
-                name="message"
-              ></textarea>
-            </div>
-            <div>
-              <input
-                type="submit"
-                className="btn--outline"
-                value="Send Message"
-              ></input>
-            </div>
-          </div>
-        </form>
+          </form>
+        </div>
       </div>
       <Footer />
-    </div>
+    </>
   );
 }
